149129a7bd925ea159408b86263d5dc5d8c875c6
TerrTarr1 <80401520+TerrTarr1@users.noreply.github.com>
2022-06-15 17:03:41 +0900

    Create AWS_user.tf

---
+++ AWS_user.tf
@@ -0,0 +1,8 @@
+resource "aws_key_pair" "Test_EC2_key" {
+  key_name = "TF_Test_Key"
+  public_key = file("./testPubkey.pub")
+}
+
+data "template_file" "Test_User_Data" {
+  template = file("Test_Web.sh")
+}
