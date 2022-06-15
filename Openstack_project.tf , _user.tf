9f7f68404249056927ae813a2f03b70a9b39b37b
TerrTarr1 <80401520+TerrTarr1@users.noreply.github.com>
2022-06-15 17:25:12 +0900

    Create Openstack_project.tf , _user.tf

---
+++ Openstack_project.tf , _user.tf
@@ -0,0 +1,36 @@
+vim project.tf
+###################################################### create project
+
+resource "openstack_identity_project_v3" "test_project" {
+        name = "test_project"
+        description = "project for test"
+}
+
+:wq
+tf plan
+tf apply
+
+
+
+vim user.tf
+############################################################# create user
+
+resource "openstack_identity_user_v3" "test_admin" {
+        default_project_id = "${openstack_identity_project_v3.test_project.id}"
+        name = "test_admin"
+        description = "admin user for test_project"
+
+        password = "1234"
+
+        ignore_change_password_upon_first_use = true
+
+        multi_factor_auth_enabled = false
+
+        extra = {
+                email = "testAdmin@labs.local"
+        }
+}
+
+:wq
+tf plan
+tf apply
