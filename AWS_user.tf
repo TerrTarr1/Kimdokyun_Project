243bef44e25319785a31a8795ae4b13d6e571451
TerrTarr1 <80401520+TerrTarr1@users.noreply.github.com>
2022-06-15 17:14:28 +0900

    Update AWS_user.tf

--- AWS_user.tf
+++ AWS_user.tf
@@ -1,3 +1,5 @@
+vim user.tf
+
 resource "aws_key_pair" "Test_EC2_key" {
   key_name = "TF_Test_Key"
   public_key = file("./testPubkey.pub")
@@ -6,3 +8,7 @@ resource "aws_key_pair" "Test_EC2_key" {
 data "template_file" "Test_User_Data" {
   template = file("Test_Web.sh")
 }
+
+:wq
+tf plan
+tf apply
